f1c90706bc6922fe58fee9d45132732fea069aa8
arunprasad017 <21147125+ArunPrasad017@users.noreply.github.com>
2021-09-05 00:34:00 -0700

    Added init commit for Flask app

--- src/app.py
+++ src/app.py
@@ -4,17 +4,23 @@ import requests
 
 app = Flask(__name__)
 
-ACCESS_TOKEN = os.environ.get('strava_access_key')
-URL = 'https://www.strava.com/api/v3/athlete'
+ACCESS_TOKEN = os.environ.get("strava_access_key")
+URL = "https://www.strava.com/api/v3/athlete"
+
 
 @app.route("/")
 def app_main():
+    """
+    Flask main function
+    """
     return "Flask app init"
 
+
 def return_resp():
-    headers_auth = {
-    'Authorization': f'Bearer {ACCESS_TOKEN}'
-    }
+    """
+    Temp function
+    """
+    headers_auth = {"Authorization": f"Bearer {ACCESS_TOKEN}"}
     print(headers_auth)
-    response = requests.get(URL,headers=headers_auth)
+    response = requests.get(URL, headers=headers_auth)
     return response.content
