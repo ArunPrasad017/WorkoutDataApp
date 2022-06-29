950efc9232a99be75535b8106036fa56652f5e55
Stefano Gallotti <stgallotti@egencia.com>
2022-06-28 18:30:47 -0700

    introduce patch with simple test

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,6 +1,14 @@
+from unittest.mock import MagicMock, patch
+
 from src.auth import refresh_access_token
 
 
+class MockPost(MagicMock):
+    def json(self):
+        return {"access_token": "FAKE_TOKEN"}
+
+
 def test_refresh_access_token():
-    refresh_access_token(None, None, None)
-    assert False
+    with patch("requests.post", MockPost):
+        access_token = refresh_access_token(None, None, None)
+        assert access_token == "FAKE_TOKEN"
